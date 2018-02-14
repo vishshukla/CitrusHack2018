bafb45f0a551e6de806dbdf4866cc1d3d512ba5d
vishshukla <vishwas.shukla@outlook.com>
2018-02-13 20:33:59 -0800

    fixed scrolling to certain sections.

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top - 54)
+          scrollTop: (target.offset().top)
         }, 1000, "easeInOutExpo");
         return false;
       }
@@ -28,13 +28,13 @@
 
 })(jQuery); // End of use strict
 
-$(document).ready(function() {
-	$(window).scroll(function() {
-  	if($(document).scrollTop() > 10) {
-    	$('#nav').addClass('shrink');
-    }
-    else {
-    $('#nav').removeClass('shrink');
-    }
-  });
-});
+// $(document).ready(function() {
+// 	$(window).scroll(function() {
+//   	if($(document).scrollTop() > 10) {
+//     	$('#nav').addClass('shrink');
+//     }
+//     else {
+//     $('#nav').removeClass('shrink');
+//     }
+//   });
+// });
