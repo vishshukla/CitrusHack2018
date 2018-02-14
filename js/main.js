bfb6b5d98ac089e63c69414855314013437b1adb
Patrick Tumbucon <ptumb001@ucr.edu>
2018-02-13 19:33:08 -0800

    Added logos, temp switch color schemes, slight heading changes, linked footer icons

--- js/main.js
+++ js/main.js
@@ -38,3 +38,13 @@ $(document).ready(function() {
     }
   });
 });
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() >= 50) {
+    $('#mainNav').removeClass('bg-dark');
+    $('#mainNav').animate({background: rgba(54, 25, 25, .5)}); 
+  } else {
+    $('#mainNav').animate({background-color: '#343a40 !important'});
+    $('#mainNav').addClass('bg-dark');
+  }
+});
